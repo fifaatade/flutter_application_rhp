50868c652e9d36735c61e74c3e707534c54b554c
fifaatade <fifaatade2020@gmail.com>
2024-04-02 09:26:35 +0100

    start enterprise page

---
+++ lib/views/entreprise/enterprisePage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:easy_sidemenu/easy_sidemenu.dart';
+
+
+class EnterprisePage extends StatefulWidget {
+  const EnterprisePage({super.key});
+
+  @override
+
+  State<EnterprisePage> createState() => _EnterprisePageState();
+}
+
+class _EnterprisePageState extends State<EnterprisePage> {
+  @override
+    void initState() {
+    sideMenu.addListener((index) {
+      pageController.jumpToPage(index);
+    });
+    super.initState();
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.all(0),
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.menu_sharp),
+                color: Color.fromARGB(255, 234, 231, 231),
+              ),
+              trailing: Container(
+                width: 30,
+                height: 40,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: const DecorationImage(
+                      image: AssetImage("assets/images/profile.jpg"),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+        ),
+    );
+  }
+}
