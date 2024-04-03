f02ec724e903155e6df2e30f13480543fa7ecd8f
fifaatade <fifaatade2020@gmail.com>
2024-04-03 11:12:19 +0100

    add side menu

--- lib/views/entreprise/enterprisePage.dart
+++ lib/views/entreprise/enterprisePage.dart
@@ -12,7 +12,11 @@ class EnterprisePage extends StatefulWidget {
 
 class _EnterprisePageState extends State<EnterprisePage> {
   @override
-    void initState() {
+    PageController pageController = PageController();
+  SideMenuController sideMenu = SideMenuController();
+
+  @override
+  void initState() {
     sideMenu.addListener((index) {
       pageController.jumpToPage(index);
     });
@@ -27,11 +31,159 @@ class _EnterprisePageState extends State<EnterprisePage> {
           children: [
             ListTile(
               contentPadding: EdgeInsets.all(0),
-              leading: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.menu_sharp),
-                color: Color.fromARGB(255, 234, 231, 231),
+              leading: SideMenu(
+            controller: sideMenu,
+            style: SideMenuStyle(
+              // showTooltip: false,
+              displayMode: SideMenuDisplayMode.auto,
+              showHamburger: true,
+              hoverColor: Colors.blue[100],
+              selectedHoverColor: Colors.blue[100],
+              selectedColor: Colors.lightBlue,
+              selectedTitleTextStyle: const TextStyle(color: Colors.white),
+              selectedIconColor: Colors.white,
+              // decoration: BoxDecoration(
+              //   borderRadius: BorderRadius.all(Radius.circular(10)),
+              // ),
+              // backgroundColor: Colors.grey[200]
+            ),
+            title: Column(
+              children: [
+                ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    maxHeight: 150,
+                    maxWidth: 150,
+                  ),
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                  ),
+                ),
+                const Divider(
+                  indent: 8.0,
+                  endIndent: 8.0,
+                ),
+              ],
+            ),
+            footer: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.lightBlue[50],
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+                  child: Text(
+                    'mohada',
+                    style: TextStyle(fontSize: 15, color: Colors.grey[800]),
+                  ),
+                ),
               ),
+            ),
+            items: [
+              SideMenuItem(
+                title: 'Dashboard',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(Icons.home),
+                badgeContent: const Text(
+                  '3',
+                  style: TextStyle(color: Colors.white),
+                ),
+                tooltipContent: "This is a tooltip for Dashboard item",
+              ),
+              SideMenuItem(
+                title: 'Users',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(Icons.supervisor_account),
+              ),
+              SideMenuExpansionItem(
+                title: "Expansion Item",
+                icon: const Icon(Icons.kitchen),
+                children: [
+                  SideMenuItem(
+                    title: 'Expansion Item 1',
+                    onTap: (index, _) {
+                      sideMenu.changePage(index);
+                    },
+                    icon: const Icon(Icons.home),
+                    badgeContent: const Text(
+                      '3',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    tooltipContent: "Expansion Item 1",
+                  ),
+                  SideMenuItem(
+                    title: 'Expansion Item 2',
+                    onTap: (index, _) {
+                      sideMenu.changePage(index);
+                    },
+                    icon: const Icon(Icons.supervisor_account),
+                  )
+                ],
+              ),
+              SideMenuItem(
+                title: 'Files',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(Icons.file_copy_rounded),
+                trailing: Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 6.0, vertical: 3),
+                      child: Text(
+                        'New',
+                        style: TextStyle(fontSize: 11, color: Colors.grey[800]),
+                      ),
+                    )),
+              ),
+              SideMenuItem(
+                title: 'Download',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(Icons.download),
+              ),
+              SideMenuItem(
+                builder: (context, displayMode) {
+                  return const Divider(
+                    endIndent: 8,
+                    indent: 8,
+                  );
+                },
+              ),
+              SideMenuItem(
+                title: 'Settings',
+                onTap: (index, _) {
+                  sideMenu.changePage(index);
+                },
+                icon: const Icon(Icons.settings),
+              ),
+              // SideMenuItem(
+              //   onTap:(index, _){
+              //     sideMenu.changePage(index);
+              //   },
+              //   icon: const Icon(Icons.image_rounded),
+              // ),
+              // SideMenuItem(
+              //   title: 'Only Title',
+              //   onTap:(index, _){
+              //     sideMenu.changePage(index);
+              //   },
+              // ),
+              const SideMenuItem(
+                title: 'Exit',
+                icon: Icon(Icons.exit_to_app),
+              ),
+            ],
+          ),
               trailing: Container(
                 width: 30,
                 height: 40,
